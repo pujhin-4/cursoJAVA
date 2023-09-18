3679012c32bc23564c62e2791ddf4dbd12c16787
Pamela Cruz <117556345+pmlcrz@users.noreply.github.com>
2023-09-18 15:56:06 -0300

    Create atividade2.java

---
+++ atividade2.java
@@ -0,0 +1,8 @@
+  int idade;//int - tipo de dado inteiro
+        String nome; //String - tipo de dado alfa-numérico
+        
+        idade = 30;
+        nome = "Pamela";
+        
+        System.out.println("Nome = "+nome+ "idade = "+idade);
+        
