9ece4b4a362c6bc84386ebc3072c9b22ddcb6997
Pamela Cruz <117556345+pmlcrz@users.noreply.github.com>
2023-09-18 16:20:55 -0300

    Delete atividade3.java

--- atividade3.java
+++
@@ -1,8 +0,0 @@
-  int valor1, valor2, soma;
-        
-        valor1 = 150;
-        valor2 = 200;
-        soma = valor1 + valor2;
-        
-        System.out.println("Soma de 150 e 200 = "+soma);
-        
